3b35f3620a52c538114cfbd0816e72abe82699e1
mabedalazim1 <mabedalazim1@gmail.com>
2021-10-21 21:21:42 +0200

    edit server

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,51 @@
 var path = require('path')
+const fetch = require('node-fetch')
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const cors = require('cors')
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+// Const Api var
+const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?'
+const apiKey = process.env.API_KEY
+console.log(apiKey)
+let inputData = ['https://github.com/mabedalazim1/evaluate-news-nlp']
+
+app.get('/',  (req, res)=> {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.get('/test',  (req, res)=> {
+    res.send(mockAPIResponse)
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+//Post Api
+app.post('/api', async (res, req) => {
+    //inputData = req.body.url
+    console.log(`You entered: ${inputData}`)
+    const nlpApiURL = `${baseUrl}key=${apiKey}&url=${inputData}&lang=en`
+    const response = await fetch(nlpApiURL)
+    const data = await response.json()
+    console.log(data)
+    res.send(data)
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8082,  () => {
+    console.log('Example app listening on port 8082!')
 })
